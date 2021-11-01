065596d5b9a01d7d17f444270cec20662f2ffcc3
ArC48 <beqa3352@gmail.com>
2021-11-01 16:44:40 +0400

    done

---
+++ shared/enums/messages.enum.ts
@@ -0,0 +1,6 @@
+export enum messages{
+    success = "Transaction ended Successfully!",
+    firstError ="Error on the first step, no Changes!",
+    restoreComplete = "Failed, but restore worked Successfully!",
+    fail = "Restore Failed!"
+}
